19b924baa045e7842d1d89e5eaae826a59f0aeb6
Gabriela Czarnowicz <82702@student.pb.edu.pl>
2023-10-10 13:18:03 +0200

    zmiana

--- app/src/main/java/com/example/quizz/MainActivity.java
+++ app/src/main/java/com/example/quizz/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.quizz;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private Button falseButton;
     private Button nextButton;
     private TextView questionTextView;
+    private TextView answersTextView;
     private int currentIndex = 0;
+    private int correctAnswers = 0;
     private Question[] questions = new Question[]{
         new Question(R.string.q_activity, false),
         new Question(R.string.q_find_resources, false),
@@ -22,18 +26,53 @@ public class MainActivity extends AppCompatActivity {
         new Question(R.string.q_version, true)
 };
 
+    private void setNextQuestion() {
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
 
+    public class Question {
+        private int questionId;
+        private boolean trueAnswer;
+
+        public Question(int questionId, boolean trueAnswer) {
+            this.questionId = questionId;
+            this.trueAnswer = trueAnswer;
+        }
+        public int getQuestionId(){
+            return questionId;
+        }
+        public boolean isTrueAnswer(){
+            return trueAnswer;
+        }
+    }
+
+    private void checkAnswerCorrectness(boolean userAnswer){
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+        if (userAnswer == correctAnswer){
+            resultMessageId = R.string.correct_answer;
+            correctAnswers++;
+        } else {
+            resultMessageId = R.string.incorrect_answer;
+        }
+        Toast.makeText (this, resultMessageId, Toast.LENGTH_SHORT).show();
+        if (currentIndex == 4) {
+            answersTextView.setText("" + correctAnswers);
+        }
+    }
+
+@SuppressLint("MissingInflatedId")
 @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button trueButton = findViewById(R.id.button_true);
-        Button falseButton = findViewById(R.id.button_false);
-        Button nextButton = findViewById(R.id.button_next);
+        trueButton = findViewById(R.id.button_true);
+        falseButton = findViewById(R.id.button_false);
+        nextButton = findViewById(R.id.button_next);
         questionTextView = findViewById(R.id.question_text_view);
+        answersTextView = findViewById(R.id.answers_text_view);
 
-        displayQuestion(currentIndex);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,41 +94,5 @@ public class MainActivity extends AppCompatActivity {
         }
         });
         setNextQuestion();
-        displayQuestion(currentIndex);
     }
-    private void displayQuestion(int questionIndex) {
-        if (questionIndex >= 0 && questionIndex < questions.length) {
-            questionTextView.setText(questions[questionIndex].getQuestionId());
-        }
-    }
-    private void setNextQuestion() {
-        questionTextView.setText(questions[currentIndex].getQuestionId());
-    }
-
-public class Question {
-    private int questionId;
-    private boolean trueAnswer;
-
-    public Question(int questionId, boolean trueAnswer) {
-        this.questionId = questionId;
-        this.trueAnswer = trueAnswer;
-    }
-    public int getQuestionId(){
-        return questionId;
-    }
-    public boolean isTrueAnswer(){
-        return trueAnswer;
-    }
-}
-
-private void checkAnswerCorrectness(boolean userAnswer){
-    boolean correctAnswer = questions[currentIndex].isTrueAnswer();
-    int resultMessageId = 0;
-    if (userAnswer == correctAnswer){
-        resultMessageId = R.string.correct_answer;
-    } else {
-        resultMessageId = R.string.incorrect_answer;
-    }
-    Toast.makeText (this, resultMessageId, Toast.LENGTH_SHORT).show();
-}
 }
